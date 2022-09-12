a1065e1c19a5ca169ddfb8a23f5bba7aa403f990
UmmuZurak <zurakummu@gmail.com>
2022-09-11 18:53:35 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
-/*
+/**
  *the main function prints the text in the puts method
- *and returs 0
+ *and returns 0
  */
-
 int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
