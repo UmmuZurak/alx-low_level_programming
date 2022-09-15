11cd84040290d101ed69ce37715331845e5e6494
UmmuZurak <zurakummu@gmail.com>
2022-09-14 21:09:08 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,20 +5,14 @@
  * Return: 0.
  */
 
-int main(void){
-
-	char c[] = "_putchar";
-
-	int i;
-
-	for (i = 0; i < 8; i++){
-		
-		_putchar(c[i]);
-
-	}
-
-	_putchar('\n');
-	
-	return (0);
-
+int main(void)
+{
+char c[] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
 }
